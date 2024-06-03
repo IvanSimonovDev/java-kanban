d3815b5af136252d7763bd98cd41d8faa7b2be13
Ivan Simonov <Inculto@yandex.ru>
2024-06-03 23:50:07 +0300

    Changes:
    Make additional task.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import tasks.Epic;
-import tasks.Statuses;
 import tasks.SubTask;
 import tasks.Task;
 
@@ -7,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         TaskManager inMemoryTaskManager = Managers.getDefault();
+        HistoryManager inMemoryHistoryManager = inMemoryTaskManager.getHistoryManager();
 
         // Создание 2-х задач и занесение их в inMemoryTaskManager
         Task taskFst = new Task("Task_1", "Description_1", "NEW");
@@ -15,67 +15,77 @@ public class Main {
         inMemoryTaskManager.createTask(taskSnd);
 
 
-        // Создание первого эпика и 2-х его подзадач + занесение их в inMemoryTaskManager
+        // Создание первого эпика и 3-х его подзадач + занесение их в inMemoryTaskManager
         Epic epicFst = new Epic("Epic_1", "Description_3", "NEW");
         SubTask subTaskFst = new SubTask("SubTask_1", "Description_4", "NEW", epicFst.id);
         SubTask subTaskSnd = new SubTask("SubTask_2", "Description_5", "NEW", epicFst.id);
+        SubTask subTaskThd = new SubTask("SubTask_3", "Description_6", "NEW", epicFst.id);
         inMemoryTaskManager.createEpic(epicFst);
         inMemoryTaskManager.createSubTask(subTaskFst);
         inMemoryTaskManager.createSubTask(subTaskSnd);
-
-        // Создание второго эпика и его подзадачи + занесение их в inMemoryTaskManager
-        Epic epicSnd = new Epic("Epic_2", "Description_6", "NEW");
-        SubTask subTaskThd = new SubTask("SubTask_3", "Description_7", "NEW", epicSnd.id);
-        inMemoryTaskManager.createEpic(epicSnd);
         inMemoryTaskManager.createSubTask(subTaskThd);
 
-        // Печать списков эпиков, задач и подзадач
-        System.out.println(inMemoryTaskManager.getEpicsList());
-        System.out.println(inMemoryTaskManager.getTasksList());
-        System.out.println(inMemoryTaskManager.getSubTasksList());
+        // Создание второго эпика  + занесение его в inMemoryTaskManager
+        Epic epicSnd = new Epic("Epic_2", "Description_7", "NEW");
+        inMemoryTaskManager.createEpic(epicSnd);
 
-        //Изменение статусов созданных объектов, печать статусов
-        // 1) Изменение статусов taskFst и taskSnd
-        taskFst.status = Statuses.IN_PROGRESS;
-        inMemoryTaskManager.updateTask(taskFst);
-        taskSnd.status = Statuses.DONE;
-        inMemoryTaskManager.updateTask(taskSnd);
+        // Запрос созданных задач несколько раз в разном порядке.
+        inMemoryTaskManager.getTask(taskSnd.id);
+        System.out.println(inMemoryHistoryManager.getHistory());
 
-        // 2) Изменение статусов subTaskFst, subTaskSnd, subTaskThd
-        subTaskFst.status = Statuses.IN_PROGRESS;
-        inMemoryTaskManager.updateSubTask(subTaskFst);
-        subTaskSnd.status = Statuses.DONE;
-        inMemoryTaskManager.updateSubTask(subTaskSnd);
-        subTaskThd.status = Statuses.DONE;
-        inMemoryTaskManager.updateSubTask(subTaskThd);
+        inMemoryTaskManager.getTask(taskFst.id);
+        System.out.println(inMemoryHistoryManager.getHistory());
 
-        // 3) Печать статусов всех созданных подзадач/задач/эпиков
-        System.out.println(taskFst.status);
-        System.out.println(taskSnd.status);
-        System.out.println(subTaskFst.status);
-        System.out.println(subTaskSnd.status);
-        System.out.println(subTaskThd.status);
-        System.out.println(epicFst.status);
-        System.out.println(epicSnd.status);
+        inMemoryTaskManager.getSubTask(subTaskThd.id);
+        System.out.println(inMemoryHistoryManager.getHistory());
 
-        //Пробуем удалить одну из задач и один из эпиков.
-        inMemoryTaskManager.deleteTask(taskFst.id);
+        inMemoryTaskManager.getEpic(epicSnd.id);
+        System.out.println(inMemoryHistoryManager.getHistory());
+
+        inMemoryTaskManager.getTask(taskFst.id);
+        System.out.println(inMemoryHistoryManager.getHistory());
+
+        inMemoryTaskManager.getTask(taskSnd.id);
+        System.out.println(inMemoryHistoryManager.getHistory());
+
+        inMemoryTaskManager.getSubTask(subTaskThd.id);
+        System.out.println(inMemoryHistoryManager.getHistory());
+
+        inMemoryTaskManager.getSubTask(subTaskFst.id);
+        System.out.println(inMemoryHistoryManager.getHistory());
+
+        inMemoryTaskManager.getEpic(epicFst.id);
+        System.out.println(inMemoryHistoryManager.getHistory());
+
+        inMemoryTaskManager.getSubTask(subTaskSnd.id);
+        System.out.println(inMemoryHistoryManager.getHistory());
+
+        inMemoryTaskManager.getEpic(epicFst.id);
+        System.out.println(inMemoryHistoryManager.getHistory());
+
+        inMemoryTaskManager.getSubTask(subTaskFst.id);
+        System.out.println(inMemoryHistoryManager.getHistory());
+
+        inMemoryTaskManager.getTask(taskSnd.id);
+        System.out.println(inMemoryHistoryManager.getHistory());
+
+        inMemoryTaskManager.getTask(taskFst.id);
+        System.out.println(inMemoryHistoryManager.getHistory());
+
+        inMemoryTaskManager.getSubTask(subTaskThd.id);
+        System.out.println(inMemoryHistoryManager.getHistory());
+
+        inMemoryTaskManager.getSubTask(subTaskSnd.id);
+        System.out.println(inMemoryHistoryManager.getHistory());
+
+
+        //Удаление задачи, которая есть в истории, и проверка, что при печати она не будет выводиться
+        inMemoryTaskManager.deleteSubTask(subTaskSnd.id);
+        System.out.println(inMemoryHistoryManager.getHistory());
+
+        //Удаление эпика с тремя подзадачами и просмотр истории
         inMemoryTaskManager.deleteEpic(epicFst.id);
-
-        System.out.println(inMemoryTaskManager.getSubTasksList());
-        System.out.println(inMemoryTaskManager.getTasksList());
-        System.out.println(inMemoryTaskManager.getEpicsList());
-
-        inMemoryTaskManager.getTask(inMemoryTaskManager.getTasksList().getFirst().id);
-        inMemoryTaskManager.getTask(inMemoryTaskManager.getTasksList().getLast().id);
-
-        inMemoryTaskManager.getSubTask(inMemoryTaskManager.getSubTasksList().getFirst().id);
-        inMemoryTaskManager.getSubTask(inMemoryTaskManager.getSubTasksList().getLast().id);
-
-        inMemoryTaskManager.getEpic(inMemoryTaskManager.getEpicsList().getFirst().id);
-        inMemoryTaskManager.getEpic(inMemoryTaskManager.getEpicsList().getLast().id);
-
-        System.out.println(inMemoryTaskManager.getHistoryManager().getHistory());
+        System.out.println(inMemoryHistoryManager.getHistory());
 
 
     }
